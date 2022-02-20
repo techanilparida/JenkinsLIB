1ab0e27675699fb4dca62f6f6e0871150cbc06b4
Anil Parida <74540849+techanilparida@users.noreply.github.com>
2022-02-20 12:02:42 +0530

    Update common.groovy
    n

--- vars/common.groovy
+++ vars/common.groovy
@@ -5,5 +5,5 @@ def build(){
 sh 'mvn package'
 }
 def deploy(ip,appname){
-sh "scp /home/ubuntu/.jenkins/workspace/Lib1Job/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+sh "scp /home/ubuntu/.jenkins/workspace/Lib1Job/webapp/target/webapp.war ubuntu@${ip}/var/lib/tomcat9/webapps/${appname}.war"
 }
